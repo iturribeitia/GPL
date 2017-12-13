b1be8abc9331ee50217546a59e989063f70367e1
Marcos Iturribeitia <iturribeitia@gmail.com>
2017-12-13 17:27:42 -0500

    Add this new extension: public static List<string> ToCSV(this IDataReader dataReader, bool includeHeaderAsFirstRow, string separator)

--- GPL/Extensions.cs
+++ GPL/Extensions.cs
@@ -812,9 +812,6 @@ namespace GPL
             }
         }
 
-
-
-
         /// <summary>
         /// compare two DataTables and determine if the following is true:
         /// Is the number of data columns the same in both DataTables
@@ -842,7 +839,75 @@ namespace GPL
 
 
         }
+
         #endregion DataTable
+
+        #region IDataReader
+
+        /// <summary>
+        /// DataReader to CSV.
+        /// </summary>
+        /// <param name="dataReader">The data reader.</param>
+        /// <param name="includeHeaderAsFirstRow">if set to <c>true</c> [include header as first row].</param>
+        /// <param name="separator">The separator.</param>
+        /// <returns></returns>
+        public static List<string> ToCSV(this IDataReader dataReader, bool includeHeaderAsFirstRow, string separator)
+        {
+            // source: http://www.extensionmethod.net/2085/csharp/list-string/datareader-to-csv
+            // TODO Copy this method and replace the 'csvRows' for a method to write the sb.ToString() to a file.
+            List<string> csvRows = new List<string>();
+            StringBuilder sb = null;
+
+            if (includeHeaderAsFirstRow)
+            {
+                sb = new StringBuilder();
+                for (int index = 0; index < dataReader.FieldCount; index++)
+                {
+                    if (dataReader.GetName(index) != null)
+                        sb.Append(dataReader.GetName(index));
+
+                    if (index < dataReader.FieldCount - 1)
+                        sb.Append(separator);
+                }
+                csvRows.Add(sb.ToString());
+            }
+
+            while (dataReader.Read())
+            {
+                sb = new StringBuilder();
+                for (int index = 0; index < dataReader.FieldCount - 1; index++)
+                {
+                    if (!dataReader.IsDBNull(index))
+                    {
+                        string value = dataReader.GetValue(index).ToString();
+                        if (dataReader.GetFieldType(index) == typeof(String))
+                        {
+                            //If double quotes are used in value, ensure each are replaced but 2.
+                            if (value.IndexOf("\"") >= 0)
+                                value = value.Replace("\"", "\"\"");
+
+                            //If separtor are is in value, ensure it is put in double quotes.
+                            if (value.IndexOf(separator) >= 0)
+                                value = "\"" + value + "\"";
+                        }
+                        sb.Append(value);
+                    }
+
+                    if (index < dataReader.FieldCount - 1)
+                        sb.Append(separator);
+                }
+
+                if (!dataReader.IsDBNull(dataReader.FieldCount - 1))
+                    sb.Append(dataReader.GetValue(dataReader.FieldCount - 1).ToString().Replace(separator, " "));
+
+                csvRows.Add(sb.ToString());
+            }
+            dataReader.Close();
+            sb = null;
+            return csvRows;
+        }
+
+        #endregion IDataReader
     }
 
     /// <summary>
