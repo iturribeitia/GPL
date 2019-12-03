3b25a23a456ddb5285589510ce54fa88e1bbe1c3
Marcos Iturribeitia <Iturribeitia@gmail.com>
2019-12-03 14:09:08 -0500

    Add the ToDataTable extension

--- GPL/Extensions.cs
+++ GPL/Extensions.cs
@@ -49,6 +49,7 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
+using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -589,6 +590,44 @@ namespace GPL
         public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source)
         {
             return new HashSet<T>(source);
+        }
+
+        /// <summary>
+        /// Convert the current IEnumerable<T> to a DataTable.
+        /// </summary>
+        /// <typeparam name="T">Type of IEnumerable<T></typeparam>
+        /// <param name="items">This IEnumerable<T></param>
+        /// <returns></returns>
+        public static DataTable ToDataTable<T>(this IEnumerable<T> items)
+        {
+            // Create the result table, and gather all properties of a T        
+            DataTable table = new DataTable(typeof(T).Name);
+            PropertyInfo[] props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            // Add the properties as columns to the datatable
+            foreach (var prop in props)
+            {
+                Type propType = prop.PropertyType;
+
+                // Is it a nullable type? Get the underlying type 
+                if (propType.IsGenericType && propType.GetGenericTypeDefinition().Equals(typeof(Nullable<>)))
+                    propType = new NullableConverter(propType).UnderlyingType;
+
+                table.Columns.Add(prop.Name, propType);
+            }
+
+            // Add the property values per T as rows to the datatable
+            foreach (var item in items)
+            {
+                var values = new object[props.Length];
+                for (var i = 0; i < props.Length; i++)
+                    values[i] = props[i].GetValue(item, null);
+
+                table.Rows.Add(values);
+            }
+
+            return table;
+
         }
 
         #endregion this IEnumerable<T>
